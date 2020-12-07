6a824753f8b6fd3d7b8aaeeacacba2394d0639a2
ASentientBanana <asentientbanana@gmail.com>
2020-12-08 00:11:38 +0100

    added grid and some starting logic (babby steps)

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import GridTile from '../GridTile/GridTile'
 import './Grid.css'
 
 const Grid = () => {
-    const x: number[] = [];
-    for (let i = 0; i < 100; i++) {
-        x.push(i)
+    const [graph,setGraph] = useState<number[][]>([
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0]
+    ]);
+    useEffect(()=>{},[graph]);
+    const changeTile= (row:number,col:number)=>{
+        graph[row][col] = 1;
+        if(col !== graph[0].length-1) graph[row][col+1] = 1;
+        if(col !== 0) graph[row][col-1] = 1;
+        if(row !== graph.length-1) graph[row+1][col] = 1;
+        if(row !== 0) graph[row-1][col] = 1;
+        setGraph([...graph])
     }
     return (
         <div id="Grid">
-            {x.map(x => <GridTile index={x} />)}
+            {graph.map((g,row)=>g.map((tile,col)=> <GridTile key={Math.random()*100000} tile={tile} row={row} col={col} callback={changeTile}/>))}
         </div>
     );
 }
--- src/components/GridTile/GridTile.tsx
+++ src/components/GridTile/GridTile.tsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './GridTile.css';
 
-const GridTile = ({ index }: any) => {
-    const [row, setRow] = useState(0);
+const GridTile = ({ row , col ,callback,tile}: any) => {
     let isBarier: boolean = false;
-    useEffect(() => {
-        setRow(Math.floor(index / 10) + 1);
-    }, [isBarier]);
+    useEffect(()=>{
+        
+    },[isBarier]);
     const checkNeighbours = () => {
-
+        callback(row,col);
+        isBarier= true;
     }
-    return (
-        <div id="grid-tile">{`row  ${row}`}</div>
-    );
+return  <div id="grid-tile" onClick={checkNeighbours}>{tile}</div>
 }
 export default GridTile
